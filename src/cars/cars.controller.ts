8762518c90f824e0308dbd134c8334670c4888fa
Leandro Garzon <hekoej@gmail.com>
2024-09-20 21:56:17 -0500

    Borrar un carro

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -34,11 +34,8 @@ export class CarsController {
   }
 
   @Delete(':id')
-  deleteCar( @Param('id', ParseIntPipe ) id: number ) {
-    return {
-      method: 'delete',
-      id: id
-    };
+  deleteCar( @Param('id', ParseUUIDPipe ) id: string){
+    return this.carsService.delete( id );
   }
 
 
